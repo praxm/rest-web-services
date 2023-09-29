a89e3597c98a4bc8c7272bcdbd510fc8c3a49c09
praxm <praveen.kgec@gmail.com>
2023-09-29 07:58:30 +0530

    Adding Spring Security basic authentication

--- src/main/java/com/praxy/restwebservices/config/AppConfig.java
+++ src/main/java/com/praxy/restwebservices/config/AppConfig.java
@@ -5,6 +5,9 @@ import com.praxy.restwebservices.service.UserDaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class AppConfig {
@@ -12,5 +15,15 @@ public class AppConfig {
     @Bean
     public UserDaoService userDaoService(@Autowired UserRepository userRepository){
         return new UserDaoService(userRepository);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.authorizeHttpRequests(
+                auth -> auth.anyRequest().authenticated()
+        );
+        httpSecurity.httpBasic(Customizer.withDefaults());
+        return httpSecurity.build();
     }
 }
